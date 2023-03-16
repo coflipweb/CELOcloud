f87c1071718f63610a4361cfe5ff0cc243afb7cf
federico suarez <fedesuarez16@gmail.com>
2023-03-16 17:06:39 -0300

    tests

---
+++ celo-hardhat/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC, // line 25
+        path: "m/44'/60'/0'/0", // line 26
+      },
+      chainId: 44787,
+    },
+  },
+};
+
